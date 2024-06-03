0f2a8ec534ecf9abd8166fd7006cf4de218a6936
tiendatmagic <tiendatmagic8@gmail.com>
2024-06-03 23:16:27 +0700

    fix mobile

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  isDark: boolean = false;
+  toggleDarkTheme() {
+    this.isDark = !this.isDark;
+  }
 }
