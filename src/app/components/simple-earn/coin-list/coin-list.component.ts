7f3aa3662bb15712e5b6679aa2c713f1d03525e9
tiendatmagic <tiendatmagic8@gmail.com>
2024-06-17 05:21:16 +0700

    update mission

--- src/app/components/simple-earn/coin-list/coin-list.component.ts
+++ src/app/components/simple-earn/coin-list/coin-list.component.ts
@@ -9,21 +9,30 @@ import { EarnService } from '../../../services/earn.service';
   styleUrl: './coin-list.component.scss'
 })
 export class CoinListComponent {
-
+  mission: number = 0;
 
   constructor(public dialog: MatDialog, private earnService: EarnService) {
   }
-  openRegister() {
+  openRegister(getMission: number = this.mission) {
+    console.log();
     this.dialog.closeAll();
     if (this.earnService.myAddress && this.earnService.myAddress.address && this.earnService.myAddress.address.length == 42) {
       this.dialog.open(SimpleEarnRegisterModalComponent, {
         disableClose: true,
         width: '90%',
         maxWidth: '722px',
+        data: {
+          mission: getMission
+        }
       });
     }
     else {
       this.earnService.showModal("", "Bạn chưa nhập địa chỉ ví để tham gia", "error", true);
     }
+  }
+
+  ngDoCheck() {
+    this.mission = this.earnService.myAddress.mission;
+    console.log(this.mission);
   }
 }
